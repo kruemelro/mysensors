db2f34e41018b0f3f2ce5198a38b1d480b5832c6
kruemelro <p.schlarb@mailec.de>
2017-07-28 09:54:50 +0200

    Switched to Ethernet-Gateway

--- mys_esp-gateway/mys_esp-gateway.ino
+++ mys_esp-gateway/mys_esp-gateway.ino
@@ -70,44 +70,15 @@
 #define MY_RADIO_NRF24
 //#define MY_RADIO_RFM69
 
-#define MY_RF24_CHANNEL 124
+// RFA24_PA_MAX get ACK Probs
 #define MY_RF24_PA_LEVEL RF24_PA_HIGH
-
-#define MY_GATEWAY_MQTT_CLIENT
-#define MY_GATEWAY_ESP8266
-
-// Set this node's subscribe and publish topic prefix
-#define MY_MQTT_PUBLISH_TOPIC_PREFIX "mygateway-out"
-#define MY_MQTT_SUBSCRIBE_TOPIC_PREFIX "mygateway-in"
-
-// Set MQTT client id
-#define MY_MQTT_CLIENT_ID "ESP-Gateway"
-
-// Enable these if your MQTT broker requires usenrame/password
-//#define MY_MQTT_USER "username"
-//#define MY_MQTT_PASSWORD "password"
 
 // Set WIFI SSID and password
 #define MY_ESP8266_SSID "xx"
 #define MY_ESP8266_PASSWORD "xxx"
 
-// Set the hostname for the WiFi Client. This is the hostname
-// it will pass to the DHCP server if not static.
  #define MY_ESP8266_HOSTNAME "MyS-Gateway"
 
-// Enable MY_IP_ADDRESS here if you want a static ip address (no DHCP)
-// #define MY_IP_ADDRESS 192,168,178,87
-
-// If using static ip you need to define Gateway and Subnet address as well
-#define MY_IP_GATEWAY_ADDRESS 192,168,178,1
-#define MY_IP_SUBNET_ADDRESS 255,255,255,0
-
-
-// MQTT broker ip address.
-#define MY_CONTROLLER_IP_ADDRESS 172, 16, 0, 2
-
-// The MQTT broker port to to open
-#define MY_PORT 1883
 
 /*
 // Enable inclusion mode
@@ -135,10 +106,15 @@
 
 
 #include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WifiManager.h>
 #include <MySensors.h>
 
 void setup()
 {
+  WiFiManager wifiManager;
+  wifiManager.autoConnect();
 }
 
 void presentation()
